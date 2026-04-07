c604e6ca99535d9ac1625c7a7bc9f5c76ec66201
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-04-07 14:36:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
+import streamlit as st
+import pandas as pd
+import folium
+from streamlit_folium import st_folium
+from folium.plugins import MarkerCluster
 from Parcours_app import afficher_parcours
 from Skills_app import afficher_competences
-from Project_app import afficher_projet
-from Experiences_app import afficher_experience
+from streamlit_option_menu import option_menu 
+
 # 1. Configuration de la page
 st.set_page_config(page_title="Interactive CV - FAGBEDJI G. Camille Boris", layout="wide")
-
-from streamlit_option_menu import option_menu 
 
 def afficher_accueil():
     st.title("👋 Welcome on my Interactive CV.")
@@ -18,6 +21,30 @@ def afficher_accueil():
     col1, col2 = st.columns(2)
     col1.metric("Experience", "2+ ans")
     col2.metric("Projets IA", "10+")
+
+def afficher_projet():
+    st.header('🌍 My Projects')
+    try:
+        project_df = pd.read_excel("Projects.xlsx")
+        m = folium.Map(location=[9.21, 2.37], zoom_start=8)
+        cluster = MarkerCluster().add_to(m)
+        for index, p in project_df.iterrows():
+            popup_html = f""""""
+        <div style="font-family: Arial; width: 200px;">
+            <b style="color: #2E86C1;">{p['Project_name']}</b><br>
+            <small><b>Model:</b> {p['Model_type']}</small><br>
+            <p style="font-size: 12px;">{p['Key_results']}</p>
+            <p style="font-size: 10px; color: gray;">{p['Tech_stack']}</p>
+            <a href="{p['GitHub']}" target="_blank" style="color: #E67E22; text-decoration: none; font-weight: bold;">
+                See the project 🚀
+            </a> </div>
+        """
+            folium.Marker(location=[p['Lat'], p['Long']], popup=folium.Popup(popup_html, max_width=250),
+                       icon=folium.Icon(color="blue", icon="info-sign")).add_to(cluster)
+        st_folium(m, width=1200, height=500)
+    except Exception as e:
+           st.error(f"Loading error: {e}")
+
 
 ## Menu 
 selected = option_menu(menu_title = None, 
