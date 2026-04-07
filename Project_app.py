a24797e022dd7a44be1e93b8956caebb065b0a7e
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-04-07 14:30:53 +0100

    Update Project_app.py

--- Project_app.py
+++ Project_app.py
@@ -4,8 +4,6 @@ import folium
 from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
 
-st.set_page_config(page_title="Geospatial Portfolio - Boris", layout="wide")
-
 def afficher_projet():
     st.header('🌍 My Projects')
     try:
@@ -13,9 +11,8 @@ def afficher_projet():
         
         m = folium.Map(location=[9.21, 2.37], zoom_start=8)
         cluster = MarkerCluster().add_to(m)
-        
         for index, p in project_df.iterrows():
-            popup_html = f"""
+            popup_html = f""""""
         <div style="font-family: Arial; width: 200px;">
             <b style="color: #2E86C1;">{p['Project_name']}</b><br>
             <small><b>Model:</b> {p['Model_type']}</small><br>
@@ -23,11 +20,10 @@ def afficher_projet():
             <p style="font-size: 10px; color: gray;">{p['Tech_stack']}</p>
             <a href="{p['GitHub']}" target="_blank" style="color: #E67E22; text-decoration: none; font-weight: bold;">
                 See the project 🚀
-            </a>
-        </div>
+            </a> </div>
         """
-              folium.Marker(location=[p['Lat'], p['Long']], popup=folium.Popup(popup_html, max_width=250),
-              icon=folium.Icon(color="blue", icon="info-sign")).add_to(cluster)
+            folium.Marker(location=[p['Lat'], p['Long']], popup=folium.Popup(popup_html, max_width=250),
+                       icon=folium.Icon(color="blue", icon="info-sign")).add_to(cluster)
         st_folium(m, width=1200, height=500)
     except Exception as e:
            st.error(f"Loading error: {e}")
