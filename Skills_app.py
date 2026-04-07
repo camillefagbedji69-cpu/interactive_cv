9cc124597b4ee4df23514add78d6597f14f316b2
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-04-07 13:53:46 +0100

    Update Skills_app.py

--- Skills_app.py
+++ Skills_app.py
@@ -4,22 +4,19 @@ import folium
 import plotly.express as px
 
 st.set_page_config(page_title="Geospatial Portfolio - Boris", layout="wide")
-st.header('🌍 My skills')
 
-# Correction du cache (Streamlit a mis à jour sa syntaxe)
-@st.cache_data
-def load_data(file_path):
-    # Assure-toi que ton fichier s'appelle bien Projects.xlsx
-    return pd.read_excel(file_path)
-
-# Chargement (ajoute un try/except pour éviter les crashs si le fichier manque)
-try:
-    skills_df = load_data("Skills.xlsx")
-
-    fig = px.bar(skills_df, y = "Skills", x = "Level", color = "Category", orientation='h', text='Level')
-    fig.update_traces(texttemplate='%{text}%', textposition='outside')
-    fig.update_layout(showlegend=False, yaxis={'categoryorder':'total ascending'})
-    st.subheader("Technical Proficiency")
-    st.plotly_chart(fig, use_container_width=True)
-except : 
-    st.error("Check anything are good") 
+def afficher_experience():
+    st.header('🌍 My skills')
+    @st.cache_data
+    def load_data(file_path):
+        return pd.read_excel(file_path)
+    try:
+        skills_df = load_data("Skills.xlsx")
+        
+        fig = px.bar(skills_df, y = "Skills", x = "Level", color = "Category", orientation='h', text='Level')
+        fig.update_traces(texttemplate='%{text}%', textposition='outside')
+        fig.update_layout(showlegend=False, yaxis={'categoryorder':'total ascending'})
+        st.subheader("Technical Proficiency")
+        st.plotly_chart(fig, use_container_width=True)
+    except : 
+        st.error("Check anything are good") 
