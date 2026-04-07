20b079ae11a35d6fe43bc9227a70d5877a5e6b83
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-04-07 12:55:10 +0100

    Add files via upload

---
+++ Skills_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import folium
+import plotly.express as px
+
+st.set_page_config(page_title="Geospatial Portfolio - Boris", layout="wide")
+st.header('🌍 My skills')
+
+# Correction du cache (Streamlit a mis à jour sa syntaxe)
+@st.cache_data
+def load_data(file_path):
+    # Assure-toi que ton fichier s'appelle bien Projects.xlsx
+    return pd.read_excel(file_path)
+
+# Chargement (ajoute un try/except pour éviter les crashs si le fichier manque)
+try:
+    skills_df = load_data("Skills.xlsx")
+
+    fig = px.bar(skills_df, x = "Skills", y = "Level", color = "Category")
+    st.plotly_chart(fig, use_container_width = True)
+
+    
+    
