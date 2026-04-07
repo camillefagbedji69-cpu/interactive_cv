780f4ac4a6f197b6bbb271ad06cb3e3c679c7cd8
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-04-07 14:27:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+from Parcours_app import afficher_parcours
+from Skills_app import afficher_competences
+from Project_app import afficher_projet
+from Experiences_app import afficher_experience
 # 1. Configuration de la page
 st.set_page_config(page_title="Interactive CV - FAGBEDJI G. Camille Boris", layout="wide")
 
@@ -12,7 +15,6 @@ def afficher_accueil():
     intersection of mathematical modeling, AI for scientific discovery, and complex ecological systems. 
     I'm particularly motivated by the use of nonlinear systems, probabilistic modeling and machine learning to
     understand climate–ecosystem interactions in African contexts.""")
-    st.image("Boris_Fagbedji.jpg")
     col1, col2 = st.columns(2)
     col1.metric("Experience", "2+ ans")
     col2.metric("Projets IA", "10+")
@@ -20,22 +22,16 @@ def afficher_accueil():
 ## Menu 
 selected = option_menu(menu_title = None, 
     options = ["Home", "Education", "Skills", "Projects", "Experience"], 
-                      icons = ["home", "book", "bar-chart", "map", "briefcase"])
-
-# 4. Logique d'affichage
+                      icons = ["house", "book", "bar-chart", "map", "briefcase"])
 if selected == "Home":
   afficher_accueil()
 elif selected == "Education":
-  from Parcours_app import afficher_parcours
   afficher_parcours()
 elif selected == "Skills":
-  from Skills_app import afficher_competences
   afficher_competences()
 elif selected == "Projects":
-    from Project_app import afficher_projet
     afficher_projet()
 else : 
-  from Experiences_app import afficher_experience
   afficher_experience()
 
 st.sidebar.markdown("---")
